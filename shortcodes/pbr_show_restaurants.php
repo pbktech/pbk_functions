d9e94c14b9260d33a03ee8a56cbc96e41abbc54b
jewmanfoo <jewmanfoo@jewmanfoo.com>
2019-12-12 16:03:09 -0600

    updated header

--- shortcodes/pbr_show_restaurants.php
+++ shortcodes/pbr_show_restaurants.php
@@ -1,7 +1,17 @@
 <?php
 function pbr_show_restaurants() {
 
-	$return="\n<div style='overflow:auto;background-color:#FFFFFF;'>\n<table >\n<tbody>\n<tr>\n<td style=\"text-align: center;\">\n<h5>RESTAURANT</h5>\n</td>\n<td style=\"text-align: center;\">\n<h5>EMAIL</h5>\n</td>\n<td style=\"text-align: center;\">
+	$return="\n<div style='overflow:auto;background-color:#FFFFFF;'>
+	\n<table >\n
+	<thead>
+<tr style=\"height: 65px;\">
+<td style=\"text-align: center; color: #ffffff; background-color: #f36c21; width: 994.8px; height: 65px;\" colspan=\"5\">
+<h3 style=\"color: #ffffff;\">PBK Restaurant Directory</h3>
+</td>
+</tr>
+</thead>
+<tbody>\n
+	<tr  style=\"background-color: #0e2244; color: #ffffff; text-align: center;\">\n<td style=\"text-align: center;\">\n<h5>RESTAURANT</h5>\n</td>\n<td style=\"text-align: center;\">\n<h5>EMAIL</h5>\n</td>\n<td style=\"text-align: center;\">
 <h5>PHONE</h5>\n</td>\n<td style=\"text-align: center;\">\n<h5>ADDRESS</h5>\n</td>\n<td style=\"text-align: center;\">\n<h5>GM/AGM</h5>\n</td>\n<td style=\"text-align: center;\">\n<h5>AM</h5>\n</td>\n</tr>";
 	global $wpdb;
 	$restaurants = $wpdb->get_results("SELECT * FROM pbc_pbrestaurants");
