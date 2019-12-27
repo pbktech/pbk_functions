70a75d25d8aad66e275191f7109e5933cc1eda1a
jewmanfoo <jewmanfoo@jewmanfoo.com>
2019-12-27 10:37:49 -0600

    added id to changesd div

--- modules/Schedule_LU_Closure.php
+++ modules/Schedule_LU_Closure.php
@@ -114,7 +114,7 @@ jQuery(document).ready(function() {
 	$stateChange["false"]="Off";
 	$stateChange["true"]="On";
 	if(!empty($records)){
-		$ret.="<h4>Upcoming Changes</h4><div style='height:500px;overflow: auto;'>
+		$ret.="<h4 id='uc'>Upcoming Changes</h4><div style='height:500px;overflow: auto;'>
 		<table class='table table-striped'><thead><tr><th><strong>Date/Time</strong></th><th><strong>Restaurant(s)</strong></th><th colspan='3'><strong>State Change</strong></th></thead></tr><tbody>";
 		foreach($records as $rec){
 			$data=json_decode($rec->files);
