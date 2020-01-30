89c7b8dc5a64212491ca7c95fb7c7bfd28f68abc
jewmanfoo <jewmanfoo@jewmanfoo.com>
2020-01-30 13:49:21 -0600

    another test

--- classes/Restaurant.php
+++ classes/Restaurant.php
@@ -66,7 +66,10 @@ class Restaurant {
 		if(in_array("administrator", $cu->roles) || in_array("editor", $cu->roles) || in_array("author", $cu->roles)) {
 			$this->isAboveStore=1;
 		}
-		echo "AS: " . $this->isAboveStore;
+		echo "<pre>";
+		print_r($cu->roles);
+		echo "</pre>";
+
 	}
 	public function checkRestaurantAccess() {
 		if(strpos(strtolower($_SERVER['REQUEST_URI']), 'directory')==false) {
