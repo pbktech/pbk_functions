845a3f0c005334a66c848cb4d768dee066ff40e3
jewmanfoo <jewmanfoo@jewmanfoo.com>
2020-02-07 15:35:02 -0600

    went back to ahref

--- modules/miniBarCustomers.php
+++ modules/miniBarCustomers.php
@@ -63,8 +63,7 @@ jQuery(document).ready( function () {
     </table>";
     if(file_exists($report->docSaveLocation.$fileName.date("Ymd").'.csv')) {
 			$ret.="<div>
-      <button type=\"button\" class='btn btn-warning' onclick=\"javascript:window.open=('".$report->docDownloadLocation.$fileName.date("Ymd").".csv')\">Download the file</button> 
-      This download is only valid for 30 minutes.
+      <a class='btn btn-success' href='".$report->docDownloadLocation.$fileName.date("Ymd").".csv' target='_blank')\">Download the file</a> This download is only valid for 30 minutes.
       </div>";
 		}
     "
@@ -75,6 +74,7 @@ $ret.="
 <script>
 jQuery(document).ready(function() {
   jQuery('.datePicker').datepicker({
+    maxDate: new Date(),
     dateFormat : 'mm/dd/yy'
   });
   jQuery('#restaurantPicker').select2({
