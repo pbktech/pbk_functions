7820f2dbdb4ff761acc507e8d86fa17cc27f8a41
jewmanfoo <jewmanfoo@jewmanfoo.com>
2020-02-10 11:18:32 -0600

    made media picker universal

--- classes/Restaurant.php
+++ classes/Restaurant.php
@@ -772,64 +772,7 @@ if($_GET['nhoDate']!="_new"){
 		}else {
 			$imageAdd="";
 		}
-		return "
-		<script>
-		var gk_media_init = function(selector, button_selector)  {
-    var clicked_button = false;
-
-    jQuery(selector).each(function (i, input) {
-        var button = jQuery(input).next(button_selector);
-        button.click(function (event) {
-            event.preventDefault();
-            var selected_img;
-            clicked_button = jQuery(this);
-
-            // check for media manager instance
-            if(wp.media.frames.gk_frame) {
-                wp.media.frames.gk_frame.open();
-                return;
-            }
-            // configuration of the media manager new instance
-            wp.media.frames.gk_frame = wp.media({
-                title: 'Select image',
-                multiple: false,
-                library: {
-                    type: 'image'
-                },
-                button: {
-                    text: 'Use selected image'
-                }
-            });
-
-            // Function used for the image selection and media manager closing
-            var gk_media_set_image = function() {
-                var selection = wp.media.frames.gk_frame.state().get('selection');
-
-                // no selection
-                if (!selection) {
-                    return;
-                }
-
-                // iterate through selected elements
-                selection.each(function(attachment) {
-                    var url = attachment.attributes.url;
-                    clicked_button.prev(selector).val(url);
-                });
-            };
-
-            // closing event for media manger
-            wp.media.frames.gk_frame.on('close', gk_media_set_image);
-            // image selection event
-            wp.media.frames.gk_frame.on('select', gk_media_set_image);
-            // showing media manager
-            wp.media.frames.gk_frame.open();
-        });
-   });
-};
-jQuery(document).ready(function() {
-	gk_media_init('.media-input', '.media-button');
-});
-</script>
+		return $htis->pbk_addImageSelector()."
 		<div class='container-fluid;'>
 	<form method=\"post\" action=\"admin-post.php\">
   	<input type=\"hidden\" name=\"action\" value=\"pbk_save_minibar\" />
@@ -1133,6 +1076,65 @@ AND pbc_users.id=nhoHost AND pbc_pbrestaurants.restaurantID=nhoLocation");
 		</script>
 		<div id='processingGif' style=\"display: none;text-align:center;\"><img src='" . PBKF_URL . "/assets/images/processing.gif' style='height:92px;width:92px;' /></div>
 		";
+	}
+	public function pbk_addImageSelector($media="media"){
+		return "		<script>
+				var gk_media_init = function(selector, button_selector)  {
+		    var clicked_button = false;
+
+		    jQuery(selector).each(function (i, input) {
+		        var button = jQuery(input).next(button_selector);
+		        button.click(function (event) {
+		            event.preventDefault();
+		            var selected_img;
+		            clicked_button = jQuery(this);
+
+		            // check for media manager instance
+		            if(wp.media.frames.gk_frame) {
+		                wp.media.frames.gk_frame.open();
+		                return;
+		            }
+		            // configuration of the media manager new instance
+		            wp.media.frames.gk_frame = wp.media({
+		                title: 'Select image',
+		                multiple: false,
+		                library: {
+		                    type: 'image'
+		                },
+		                button: {
+		                    text: 'Use selected image'
+		                }
+		            });
+
+		            // Function used for the image selection and media manager closing
+		            var gk_media_set_image = function() {
+		                var selection = wp.media.frames.gk_frame.state().get('selection');
+
+		                // no selection
+		                if (!selection) {
+		                    return;
+		                }
+
+		                // iterate through selected elements
+		                selection.each(function(attachment) {
+		                    var url = attachment.attributes.url;
+		                    clicked_button.prev(selector).val(url);
+		                });
+		            };
+
+		            // closing event for media manger
+		            wp.media.frames.gk_frame.on('close', gk_media_set_image);
+		            // image selection event
+		            wp.media.frames.gk_frame.on('select', gk_media_set_image);
+		            // showing media manager
+		            wp.media.frames.gk_frame.open();
+		        });
+		   });
+		};
+		jQuery(document).ready(function() {
+			gk_media_init('.".$media."-input', '.".$media."-button');
+		});
+		</script>";
 	}
 	public function buildDateSelector($field='startDate',$label="Starting Date"){
 		if(isset($_GET[$field])){$dateValue=$_GET[$field];}else{$dateValue="";}
