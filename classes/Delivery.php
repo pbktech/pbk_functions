07acaf979fe5ca88c6fb56d795588128167dbdb8
Jon Arbitman <jon@theproteinbar.com>
2020-03-19 11:44:09 -0500

    added delivery class

---
+++ classes/Delivery.php
@@ -0,0 +1,48 @@
+<?php
+/*
+*
+*
+*
+*
+*
+*
+*
+*/
+class Delivery {
+  private $provider=null;
+  private $apiKey=null;
+  private $customerID=null;
+  private $authCode=null;
+  private $requestURL;
+  private $localDB;
+  private $config;
+
+  public function __construct($p) {
+    $this->setConfig($p);
+		$this->connectDB();
+  }
+  function setConfig($p){
+		$default = dirname(ABSPATH) . '/config.json';
+		$this->config=json_decode(file_get_contents($default));
+		$this->localDB=$this->config->dBase;
+    $this->provider=$p;
+    $this->apiKey=$this->config->$p->API;
+    $this->customerID=$this->config->$p->Customer;
+    $this->customerID=$this->config->$p->URL;
+	}
+  function connectDB() {
+		$this->mysqli = new mysqli($this->config->host, $this->config->username, $this->config->password, $this->config->dBase);
+		$this->mysqli->set_charset('utf8mb4');
+	}
+  function requestPostmatesDelivery($request) {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt ($ch, CURLOPT_HTTPHEADER,Array("Content-Type: application/x-www-form-urlencoded","Authorization: Basic ".$this->apiKey));
+		curl_setopt($ch, CURLOPT_POSTFIELDS, implode("&",$request));
+		curl_setopt($ch, CURLOPT_URL,$this->requestURL. "v1/customers/" . $this->customerID ."/deliveries");
+		$result=curl_exec($ch);
+		return json_decode($result);
+	}
+
+}
