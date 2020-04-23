ce601aab3f06eaebdac23ae48eab5f75b76ce33a
Jon Arbitman <jon@theproteinbar.com>
2020-04-23 13:28:54 -0500

    fixed JS

--- classes/Restaurant.php
+++ classes/Restaurant.php
@@ -823,7 +823,7 @@ if($_GET['nhoDate']!="_new"){
 			}
 		}
 		if(isset($links['day']) && count($links['day'])!=0){
-			$preselect="jQuery(#deliveryDay).select2('data',[";
+			$preselect="jQuery('#deliveryDay').select2('data',[";
 			foreach($links['day'] as $day){
 				$preselect.="{id:'".$day."', text:'".$day."'}";
 			}
