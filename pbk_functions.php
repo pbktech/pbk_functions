ecc92157deceb3d63af1447a6a22521637ddb852
Jon Arbitman <jon@theproteinbar.com>
2020-06-18 17:07:17 -0500

    deregistering style

--- pbk_functions.php
+++ pbk_functions.php
@@ -127,3 +127,8 @@ function switchpbrMessages($m) {
   </script>
 <div class='alert alert-".$alert."'><strong>" . $ms . "</strong></div>";
 }
+add_action('wp_print_styles', 'my_deregister_styles', 100);
+
+function my_deregister_styles() {
+  wp_deregister_style('bootstrap_style');
+}
