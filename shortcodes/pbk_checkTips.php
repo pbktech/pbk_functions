148104c34a7e743cf3b5c1d43305f36e4b672fea
Jon Arbitman <jon@theproteinbar.com>
2020-06-19 09:55:16 -0500

    modal tests

--- shortcodes/pbk_checkTips.php
+++ shortcodes/pbk_checkTips.php
@@ -35,7 +35,7 @@ function pbk_CheckTips() {
     }
     pbk_show_modal($message,"<button type=\"button\" class=\"btn btn-success\" onclick=\"window.location.href='". home_url("/operations/tips/tip-distribution/")."'\">Tip Distribution</button>");
   }
-  if($cu->ID==1){
+  if(in_array("administrator", $cu->roles)){
     pbk_show_modal("Test Modal","<button type=\"button\" class=\"btn btn-success\" onclick=\"window.location.href='". home_url("/operations/tips/tip-distribution/")."'\">Tip Distribution</button>");
   }
 }
