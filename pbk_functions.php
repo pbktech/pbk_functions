cb2ae3db29180dc51e304b8d8eb157f2ccfc53c8
Jon Arbitman <jon@theproteinbar.com>
2020-06-18 22:55:58 -0500

    added login tip modal

--- pbk_functions.php
+++ pbk_functions.php
@@ -53,6 +53,44 @@ function pbk_scripts(){
 }
 add_action( 'wp_enqueue_scripts', 'pbk_scripts' );
 
+function pbk_CheckTips() {
+  $toast = new ToastReport();
+  $rests=$toast->getAvailableRestaurants();
+  $cu = wp_get_current_user();
+  if(in_array("administrator", $cu->roles) || in_array("editor", $cu->roles) || in_array("author", $cu->roles)) {
+  	continue;
+  }else {
+    $_REQUEST['rid']=$rests[0]->restaurantID;
+    $bot="2019-01-07 00:00:00";
+    $latest=date("Y-m-d",time() - 60 * 60 * 24)." 23:59:59";
+    $toast = new ToastReport($_REQUEST['rid']);
+  	$toast ->setStartTime(date("Y-m-d G:i:s",strtotime($bot)));
+  	$toast ->setEndTime(date("Y-m-d G:i:s",strtotime($latest)));
+  	$orders=$toast->getTippedOrders();
+    if(is_array($orders) && count($orders)!=0){
+      echo "
+      <script>
+        jQuery( document ).ready(function() {
+          jQuery('#tipsRequired').trigger('focus')
+        });
+      </script>
+      <div class=\"modal fade\" id=\"tipsRequired\" tabindex="\-1\" role=\"dialog\" aria-labelledby=\"tipsRequired\" aria-hidden=\"true\">
+        <div class=\"modal-dialog\" role=\"document\">
+          <div class=\"modal-content\">
+            <div class=\"modal-body\">
+              There are ".count($orders)." requiring tip assignment. <br><br>Please <a href='". home_url("/operations/tips/tip-distribution/")."'>assign</a> the tips.
+            </div>
+            <div class=\"modal-footer\">
+              <button type=\"button\" class=\"btn btn-secondary\" data-dismiss=\"modal\">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      ";
+    }
+  }
+}
+add_action('wp_login', 'pbk_CheckTips');
 
 /*Classes*/
 use PHPMailer\PHPMailer\PHPMailer;
