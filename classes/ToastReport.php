4214583cbbc97a3e832632e18b031482f0dd7f31
Jon Arbitman <jon@theproteinbar.com>
2020-06-22 07:40:18 -0500

    added getHeaderInformationFromCheck($guid)

--- classes/ToastReport.php
+++ classes/ToastReport.php
@@ -397,6 +397,16 @@ AND ToastOrderID IN (SELECT GUID FROM pbc_ToastOrderHeaders WHERE restaurantID=?
 		$row=$result->fetch_object();
 		return $row->Total;
 	}
+	function getHeaderInformationFromCheck($guid) {
+		$q="SELECT * FROM pbc2.pbc_ToastOrderHeaders,pbc2.pbc_ToastCheckHeaders,pbc_pbrestaurants WHERE
+pbc_ToastCheckHeaders.GUID ='$guid' AND pbc_ToastOrderHeaders.GUID=pbc_ToastCheckHeaders.ToastOrderID
+AND pbc_ToastOrderHeaders.restaurantID = pbc_pbrestaurants.restaurantID ";
+		$stmt = $this->mysqli->prepare($q);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		$row=$result->fetch_object();
+		return $row->Total;
+	}
 	function getNetSalesbyHour() {
 		$q="SELECT (SUM(checkAmount)-SUM(gcSold)) as 'Amount' FROM pbc2.pbc_sum_CheckSales WHERE guid IN (SELECT GUID FROM pbc2.pbc_ToastOrderHeaders WHERE openedDate between '".$this->startTime."' AND  '".$this->endTime."')";
 		$stmt = $this->mysqli->prepare($q);
