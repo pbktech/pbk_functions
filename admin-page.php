e8d6dabc09eb989a46111d530be47c5622f61d03
Jon Arbitman <jon@theproteinbar.com>
2020-08-23 11:28:48 -0500

    json test

--- admin-page.php
+++ admin-page.php
@@ -250,9 +250,9 @@ function pbr_hs_archive(){
 <script>
 $(\'#exampleModal\').on(\'show.bs.modal\', function (event) {
   var button = $(event.relatedTarget) // Button that triggered the modal
-  var recipient = button.data(\'whatever\') // Extract info from data-* attributes
+  var obj = jQuery.parseJSON(button.data(\'whatever\')) // Extract info from data-* attributes
   var modal = $(this)
-  modal.find(\'.modal-title\').text(\'New message to \' + recipient)
+  modal.find(\'.modal-title\').text(\'New message to \' + obj.name)
   modal.find(\'.modal-body input\').val(recipient)
 })
 </script>';
