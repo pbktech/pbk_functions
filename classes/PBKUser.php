be45599fd32c0c9696ff7ffc8a00a5764b133c39
Jon <jewmanfoo@jewmanfoo.com>
2020-10-09 14:52:06 -0500

    more logout fixes

--- classes/PBKUser.php
+++ classes/PBKUser.php
@@ -184,7 +184,7 @@ class PBKUser
     {
       $logOutTime=date("Y-m-d G:i:s");
       $stmt=$this->mysqli->prepare("UPDATE pbc_minibar_users_sessions SET logoutTime=? WHERE SessionGUID = UuidToBin(?)");
-      $stmt->bind_param("ss", $logOutTime,$sessionGUID);
+      $stmt->bind_param("ss", $logOutTime,$sessionID);
       $stmt->execute();
       if (isset($stmt->error) && $stmt->error!='') {
           return false;
