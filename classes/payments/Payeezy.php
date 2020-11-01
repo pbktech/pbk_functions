562e63994b547d05c4bc1012ff0a77969e4d7df0
Jon <jewmanfoo@jewmanfoo.com>
2020-11-01 15:07:27 -0600

    Adding tokenization

--- classes/payments/Payeezy.php
+++ classes/payments/Payeezy.php
@@ -33,13 +33,15 @@ class Payeezy extends PBKPayment{
                 "auth" => "false"
             ]
         );
-        $tasks=new task_engine($this->mysqli);
-        $tasks->add_task(
-            ['what'=>'execBackground',
-            'target'=>"/home/jewmanfoo/toast-api/ccFunction.php ",
-            'files' => json_encode(array("action" => "void", "transaction_tag"=> $authorize_response->transaction_tag, "transaction_id" => $authorize_response->transaction_id)),
-            'dueDate' => date('Y-m-d H:i:s', strtotime('+1 hour'))]
-        );
+        if($authorize_response->transaction_status == 'approved') {
+            $tasks = new task_engine($this->mysqli);
+            $tasks->add_task(
+                ['what' => 'execBackground',
+                    'target' => "/home/jewmanfoo/toast-api/ccFunction.php ",
+                    'files' => json_encode(array("action" => "void", "transaction_tag" => $authorize_response->transaction_tag, "transaction_id" => $authorize_response->transaction_id)),
+                    'dueDate' => date('Y-m-d H:i:s', strtotime('+1 hour'))]
+            );
+        }
         return $authorize_response;
     }
 
