4283f4b5a8b7df1d3c19c3ef79e8bd986f5f0309
Jon <jewmanfoo@jewmanfoo.com>
2020-11-01 09:59:54 -0600

    Adding tokenization

--- classes/payments/Payeezy.php
+++ classes/payments/Payeezy.php
@@ -86,7 +86,7 @@ class Payeezy extends PBKPayment{
         $request = curl_init();
         curl_setopt($request, CURLOPT_URL, $this->config->Payeezy->URL . $endpoint);
         curl_setopt($request, CURLOPT_POST, true);
-        curl_setopt($request, CURLOPT_POSTFIELDS, $payload);
+        curl_setopt($request, CURLOPT_POSTFIELDS, $json);
         curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($request, CURLOPT_HEADER, false);
         curl_setopt(
