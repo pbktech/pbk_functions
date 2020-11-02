1fec33a0539ace8bb91f39cfcc27dbc2f1a74a06
Jon <jewmanfoo@jewmanfoo.com>
2020-11-01 19:02:41 -0600

    added group link support

--- classes/payments/HouseAccount.php
+++ classes/payments/HouseAccount.php
@@ -3,9 +3,38 @@
 
 class HouseAccount extends PBKPayment{
 
+    private $guid;
+
     public function __construct($mysqli){
         parent::__construct($mysqli);
 
     }
 
+    public function validateHouseAccount(){
+        $stmt=$this->mysqli->prepare("SELECT accountID, maxIndividualOrder FROM pbc_minibar_house_accounts WHERE publicUnique=UuidToBin(?) AND onHold=0");
+        $stmt->bind_params('s',$this->guid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $row = $result->fetch_object();
+        if(!empty($row)){
+            $args=array();
+            $args['mbCheckID']=null;
+            $args['mbUserID'] = $this->userID;
+            $args['paymentType'] = "House";
+            $args['paymentDate'] = date('Y-m-d H:i:s');
+            $args['paymentAmount'] = .01;
+            $args['paymentStatus'] = "approved";
+            $args['authorization'] = json_encode(array());
+            $args['fdsToken'] = json_encode(array());
+            $args['cardNum'] = $row->accountID;
+            $args['transactionID'] = json_encode(array());
+            $args['addressID'] = $this->billingID;
+            $info=$this->addPaymentToTable($args);
+            return (object)["response" => array("transaction_status" => "approved"), "info" => $info];
+        }
+    }
+
+    public function setGUID(string $guid): void{
+        $this->guid = $guid;
+    }
 }
