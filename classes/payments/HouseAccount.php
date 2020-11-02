34fc59f44ff4711c56cbcbdbfd30eee4dfd7c92b
Jon <jewmanfoo@jewmanfoo.com>
2020-11-02 09:05:58 -0600

    added group link support

--- classes/payments/HouseAccount.php
+++ classes/payments/HouseAccount.php
@@ -11,27 +11,30 @@ class HouseAccount extends PBKPayment{
     }
 
     public function validateHouseAccount(){
-        $mysqli=$this->getMysqli();
-        $stmt=$mysqli->prepare("SELECT accountID, maxIndividualOrder FROM pbc_minibar_house_accounts WHERE publicUnique=UuidToBin(?) AND onHold=0");
-        $stmt->bind_params('s',$this->guid);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $row = $result->fetch_object();
-        if(!empty($row)){
-            $args=array();
-            $args['mbCheckID']=null;
-            $args['mbUserID'] = $this->userID;
-            $args['paymentType'] = "House";
-            $args['paymentDate'] = date('Y-m-d H:i:s');
-            $args['paymentAmount'] = .01;
-            $args['paymentStatus'] = "approved";
-            $args['authorization'] = json_encode(array());
-            $args['fdsToken'] = json_encode(array());
-            $args['cardNum'] = $row->accountID;
-            $args['transactionID'] = json_encode(array());
-            $args['addressID'] = $this->billingID;
-            $info=$this->addPaymentToTable($args);
-            return (object)["response" => array("transaction_status" => "approved"), "info" => $info];
+        if($mysqli=$this->getMysqli()) {
+            $stmt = $mysqli->prepare("SELECT accountID, maxIndividualOrder FROM pbc_minibar_house_accounts WHERE publicUnique=UuidToBin(?) AND onHold=0");
+            $stmt->bind_params('s', $this->guid);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $row = $result->fetch_object();
+            if (!empty($row)) {
+                $args = array();
+                $args['mbCheckID'] = null;
+                $args['mbUserID'] = $this->userID;
+                $args['paymentType'] = "House";
+                $args['paymentDate'] = date('Y-m-d H:i:s');
+                $args['paymentAmount'] = .01;
+                $args['paymentStatus'] = "approved";
+                $args['authorization'] = json_encode(array());
+                $args['fdsToken'] = json_encode(array());
+                $args['cardNum'] = $row->accountID;
+                $args['transactionID'] = json_encode(array());
+                $args['addressID'] = $this->billingID;
+                $info = $this->addPaymentToTable($args);
+                return (object)["response" => array("transaction_status" => "approved"), "info" => $info];
+            }else{
+                return (object)["response" => array("transaction_status" => "failed")];
+            }
         }
     }
 
