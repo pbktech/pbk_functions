d4f015f1a1f25e551044b11e6b9937bc48672269
Jon <jewmanfoo@jewmanfoo.com>
2020-11-08 13:23:05 -0600

    updated to add capture auth and capture token

--- classes/payments/Payeezy.php
+++ classes/payments/Payeezy.php
@@ -79,6 +79,7 @@ class Payeezy extends PBKPayment{
                 "currency_code" => "USD",
             )
         );
+        $reponse = json_encode(json_decode($reponse));
         $stmt = $this->mysqli->prepare("UPDATE pbc_minibar_order_payment SET capture = ? WHERE paymentID = ?");
         $stmt->bind_param("ss",$reponse, $this->paymentID);
         $stmt->execute();
