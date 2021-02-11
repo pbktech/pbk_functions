17ff55c9f2574fa571dc3cbe1de7f39401565076
Jon <jewmanfoo@jewmanfoo.com>
2021-02-11 08:38:19 -0600

    added welcome email

---
+++ templates/email/public.php
@@ -0,0 +1,136 @@
+<?php
+function getHeader(){
+    return '      <div>
+        <div style="Margin:0 auto;max-width:600px;min-width:320px;width:320px;width:calc(28000% - 167400px)">
+
+          <div style="font-size:26px;line-height:32px;Margin-top:30px;Margin-bottom:30px;color:#2f353e;font-family:PT Sans,Trebuchet MS,sans-serif;Margin-left:20px;Margin-right:20px" align="center">
+            <div id="m_1428688053685981531m_-1170683036063149700emb-email-header" align="center"><img style="display:block;height:auto;width:100%;border:0;max-width:425px" src="https://images.getbento.com/accounts/79416dac6744e5896d428cd16e2e574c/media/images/79704PBK-Logo_Primary_Full-Color_email.png?w=1800&fit=max&auto=compress,format&h=1800" alt="Protein Bar &amp; Kitchen"
+                class="CToWUd" width="425"></div>
+          </div>
+
+        </div>
+      </div>
+      <div>
+        <div style="background-color:#f36c21">
+          <div style="Margin:0 auto;max-width:600px;min-width:320px;width:320px;width:calc(28000% - 167400px);word-wrap:break-word;word-break:break-word">
+            <div style="border-collapse:collapse;display:table;width:100%">
+
+              <div style="text-align:left;color:#000;font-size:16px;line-height:24px;font-family:Lora,Palatino,Book Antiqua,Georgia,serif">
+
+                <div style="Margin-left:20px;Margin-right:20px">
+                  <div style="line-height:15px;font-size:1px">&nbsp;</div>
+                </div>
+
+                <div style="Margin-left:20px;Margin-right:20px">
+                  <div style="vertical-align:middle">
+                    <h3 style="Margin-top:0;Margin-bottom:12px;font-style:normal;font-weight:normal;color:#000;font-size:17px;line-height:26px;font-family:oswald,avenir next condensed,arial narrow,ms ui gothic,sans-serif;text-align:center"
+                      lang="x-size-18">
+                      <span><span style="color:#ffffff">
+                          <a style="text-decoration:none;color:#fff" href="https://www.theproteinbar.com/restaurants/" target="_blank">LOCATIONS</a>&nbsp; &nbsp; &nbsp;◦&nbsp; &nbsp; &nbsp;
+                          <a style="text-decoration:none;color:#fff" href="https://www.theproteinbar.com/menu/" target="_blank">MENUS</a>&nbsp; &nbsp; &nbsp;◦&nbsp;&nbsp; &nbsp; &nbsp;
+                          <a style="text-decoration:none;color:#fff" href="https://catering.theproteinbar.com/index.cfm?fuseaction=order&action=menu-categories&channel-id=2&_ga=2.140483831.557655290.1533063020-502668083.1502994418"
+                            target="_blank">CATERING</a>&nbsp; &nbsp; &nbsp;◦&nbsp; &nbsp; &nbsp;
+                          <a style="text-decoration:none;color:#fff" href="https://www.theproteinbar.com/rewards/" target="_blank">REWARDS</a>&nbsp; &nbsp; &nbsp;◦&nbsp; &nbsp; &nbsp;
+                          <a style="text-decoration:none;color:#fff" href="https://proteinbar.thelevelup.com/?fulfillment=pickup" target="_blank">ORDER ONLINE</a>
+                        </span></span>
+                    </h3>
+                  </div>
+                </div>
+
+                <div style="Margin-left:20px;Margin-right:20px">
+                  <div style="line-height:1px;font-size:1px">&nbsp;</div>
+                </div>
+
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+        <div style="background-color:#ffffff">
+          <div style="Margin:0 auto;max-width:600px;min-width:320px;width:320px;width:calc(28000% - 167400px);word-wrap:break-word;word-break:break-word">
+            <div style="border-collapse:collapse;display:table;width:100%">
+
+              <div style="text-align:left;color:#000;font-size:16px;line-height:24px;font-family:Lora,Palatino,Book Antiqua,Georgia,serif">
+';
+}
+
+function getFooter(){
+    return '</div>
+
+</div>
+</div>
+</div>
+
+<div style="Margin:0 auto;max-width:600px;min-width:320px;width:320px;width:calc(28000% - 167400px);word-wrap:break-word;word-break:break-word">
+<div style="border-collapse:collapse;display:table;width:100%;background-color:#ffffff">
+
+<div style="text-align:left;color:#000;font-size:16px;line-height:24px;font-family:Lora,Palatino,Book Antiqua,Georgia,serif">
+
+<div style="Margin-left:20px;Margin-right:20px">
+  <div style="line-height:20px;font-size:1px">&nbsp;</div>
+</div>
+
+</div>
+
+</div>
+</div>
+
+
+<div>
+<div style="Margin:0 auto;max-width:600px;min-width:320px;width:320px;width:calc(28000% - 167400px);word-wrap:break-word;word-break:break-word">
+<div style="border-collapse:collapse;display:table;width:100%">
+
+<div
+  style="text-align:left;font-size:12px;line-height:19px;color:#c2c2c2;font-family:Roboto Condensed,Arial Narrow,Avenir Next Condensed,Roboto,sans-serif;Float:left;max-width:400px;min-width:320px;width:320px;width:calc(8000% - 47600px)">
+  <div style="Margin-left:20px;Margin-right:20px;Margin-top:10px;Margin-bottom:10px">
+    <table style="border-collapse:collapse;table-layout:fixed">
+      <tbody>
+        <tr>
+          <td style="padding:0;width:26px"><a style="text-decoration:underline;color:#c2c2c2" href="https://proteinbarandkitchen.cmail20.com/t/j-l-qdkeg-jhhjijihb-u/" target="_blank"><img
+                style="border:0"
+                src="http://i2.cmail20.com/static/eb/master/13-the-blueprint-3/images/facebook.png"
+                alt="Facebook" class="CToWUd" width="26" height="26"></a></td>
+          <td style="padding:0 0 0 3px;width:26px"><a style="text-decoration:underline;color:#c2c2c2" href="https://proteinbarandkitchen.cmail20.com/t/j-l-qdkeg-jhhjijihb-o/" target="_blank"><img
+                style="border:0"
+                src="http://i3.cmail20.com/static/eb/master/13-the-blueprint-3/images/twitter.png"
+                alt="Twitter" class="CToWUd" width="26" height="26"></a></td>
+          <td style="padding:0 0 0 3px;width:26px"><a style="text-decoration:underline;color:#c2c2c2" href="https://proteinbarandkitchen.cmail20.com/t/j-l-qdkeg-jhhjijihb-b/" target="_blank"><img
+                style="border:0"
+                src="http://i5.cmail20.com/static/eb/master/13-the-blueprint-3/images/instagram.png"
+                alt="Instagram" class="CToWUd" width="26" height="26"></a></td>
+        </tr>
+      </tbody>
+    </table>
+    <div style="font-size:12px;line-height:19px;Margin-top:20px">
+      <div><span class="il">Protein</span> <span class="il">Bar</span> &amp; <span class="il">Kitchen</span><br>
+        231 S. Lasalle<br>
+        Suite 2100<br>
+        Chicago, IL 60606</div>
+    </div>
+    <div style="font-size:12px;line-height:19px;Margin-top:18px">
+      <div>You’re receiving this email because you’re a valued&nbsp;customer.</div>
+    </div>
+
+  </div>
+</div>
+
+<div
+  style="text-align:left;font-size:12px;line-height:19px;color:#c2c2c2;font-family:Roboto Condensed,Arial Narrow,Avenir Next Condensed,Roboto,sans-serif;Float:left;max-width:320px;min-width:200px;width:320px;width:calc(72200px - 12000%)">
+  <div style="Margin-left:20px;Margin-right:20px;Margin-top:10px;Margin-bottom:10px">
+
+  </div>
+</div>
+
+</div>
+</div>
+<div style="Margin:0 auto;max-width:600px;min-width:320px;width:320px;width:calc(28000% - 167400px);word-wrap:break-word;word-break:break-word">
+<div style="border-collapse:collapse;display:table;width:100%">
+
+</div>
+</div>
+</div>
+<div style="line-height:40px;font-size:40px">&nbsp;</div>
+</div>
+';
+}
