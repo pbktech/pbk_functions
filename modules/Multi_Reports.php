37a5fc689fac796556de1ad2769defaf3a2423b9
Jon <jewmanfoo@jewmanfoo.com>
2021-02-15 08:49:43 -0600

    added waitlist report

--- modules/Multi_Reports.php
+++ modules/Multi_Reports.php
@@ -1,17 +1,17 @@
 <?php
 // $reports=[N][[Title][SQL][Column Headers (SQL))][File Name]]
-if(isset($_REQUEST['startDate']) && isset($_REQUEST['endDate'])) {
+if (isset($_REQUEST['startDate']) && isset($_REQUEST['endDate'])) {
     $startDate = date("Y-m-d", strtotime($_REQUEST['startDate']));
     $endDate = date("Y-m-d", strtotime($_REQUEST['endDate']));
-}else{
+} else {
     $startDate = '';
     $endDate = '';
 }
-    $reports[1] = array("Title" => "Ticket Times", "SQL" => "SELECT restaurantName as 'Name', date_format(sent_time,'%c/%e/%Y') as Weekday,time_format(sent_time,'%h %p') as Sales_Hour , SEC_TO_TIME(avg(duration)) as Average_Wait,
+$reports[1] = array("Title" => "Ticket Times", "SQL" => "SELECT restaurantName as 'Name', date_format(sent_time,'%c/%e/%Y') as Weekday,time_format(sent_time,'%h %p') as Sales_Hour , SEC_TO_TIME(avg(duration)) as Average_Wait,
 SEC_TO_TIME(MAX(duration)) as Max_Wait, COUNT(*) as Customer_Count FROM `kds_detail`,`pbc_pbrestaurants`
 WHERE `kds_detail`.`restaurantID` = `pbc_pbrestaurants`.`restaurantID` AND sent_time BETWEEN '" . $startDate . "' AND '" . $endDate . " 23:59:59' GROUP BY `kds_detail`.restaurantID,date_format(sent_time,'%c%e%Y'),hour(sent_time) ORDER BY `kds_detail`.restaurantID,date_format(sent_time,'%c%e%Y'),hour(sent_time) ",
-        "Headers" => array("Name", "Weekday", "Sales Hour", "Average Wait", "Max Wait", "Customer Count"), "FN" => "tt"
-    );
+    "Headers" => array("Name", "Weekday", "Sales Hour", "Average Wait", "Max Wait", "Customer Count"), "FN" => "tt"
+);
 $reports[2] = array("Title" => "Tenders", "SQL" => "SELECT restaurantName as 'Name', BusinessDate as 'Date_of_Business', Tender, Count, Total FROM v_R_TendersFromArchive,pbc_pbrestaurants WHERE v_R_TendersFromArchive.restaurantID=pbc_pbrestaurants.restaurantID
 AND BusinessDate BETWEEN '" . $startDate . "' AND '" . $endDate . "';",
     "Headers" => array("Name", "Date of Business", "Tender", "Count", "Total"), "FN" => "tenders"
@@ -21,9 +21,12 @@ pbc_curbside_link pcl, pbc_ToastOrderHeaders ptoh, pbc_ToastCheckHeaders ptch, p
 WHERE ptch.ToastOrderID = ptoh.GUID AND pcl.toastCheckID = ptch.GUID AND ptci.ToastCheckID = ptch.GUID AND ptoh.restaurantID = ppr.restaurantID AND ptoh.businessDate BETWEEN '" . $startDate . "' AND '" . $endDate . "';",
     "Headers" => array("Restaurant", "Name", "Date of Business", "Check", "Amount", "Ordered", "Arrived"), "FN" => "curbside"
 );
+$reports[4] = array("Title" => "Waitlist", "SQL" => "SELECT guestName,date_format(dateAdded,'%m/%d/%Y') as 'added', emailAddress  FROM pbc_subscriptions_waitlist WHERE dateAdded BETWEEN '" . $startDate . "' AND '" . $endDate . "';",
+    "Headers" => array("Name", "Date Added", "E-mail Address"), "FN" => "waitlist"
+);
 
 
-		$ret.="
+$ret .= "
 		<script type=\"text/javascript\">
 
 jQuery(document).ready(function() {
@@ -34,7 +37,7 @@ jQuery(document).ready(function() {
 
 </script>
 		<div class='container'>
-			<form method='get' class='needs-validation' action='".site_url()."/finance/finance-reports/multi_reports/' novalidate>
+			<form method='get' class='needs-validation' action='" . site_url() . "/finance/finance-reports/multi_reports/' novalidate>
 			    <div class=\"form-row\">
 			        <div class=\"col-4\">
 			        <label for='startDate'>Report</label>
@@ -42,13 +45,15 @@ jQuery(document).ready(function() {
 						<option value=''>Choose One</option>
 						";
 
-foreach($reports as $r=>$n){
-    $checked="";
-    if(isset($_REQUEST['rpt']) && $_REQUEST['rpt']==$r) {$checked="selected";}
-    $ret.="
-						<option value='".$r."' $checked>".$n['Title']."</option>";
+foreach ($reports as $r => $n) {
+    $checked = "";
+    if (isset($_REQUEST['rpt']) && $_REQUEST['rpt'] == $r) {
+        $checked = "selected";
+    }
+    $ret .= "
+						<option value='" . $r . "' $checked>" . $n['Title'] . "</option>";
 }
-$ret.="
+$ret .= "
 					</select>
 					<div class=\"invalid-feedback\">Please select a report</div>
 			        </div>
@@ -97,27 +102,28 @@ $ret.="
 </script>
 
 		";
-if(isset($_REQUEST['endDate']) && isset($_REQUEST['startDate']) && (isset($_REQUEST['rpt']) && is_numeric($_REQUEST['rpt']))) {
-	global $wpdb;
+if (isset($_REQUEST['endDate']) && isset($_REQUEST['startDate']) && (isset($_REQUEST['rpt']) && is_numeric($_REQUEST['rpt']))) {
+    global $wpdb;
     $toast = new ToastReport();
-	if($_REQUEST['rpt']==1) {
-		$lastImport = $wpdb->get_var( "SELECT MAX(sent_time) FROM pbc2.kds_detail" );
-		$ret.="<div >The last imported date is ".date("m/d/Y", strtotime($lastImport)).".</div>";
-	}
-	$result = $wpdb->get_results($reports[$_REQUEST['rpt']]['SQL']);
-    if($result){
-        $ret.="<hr /><h4>".$reports[$_REQUEST['rpt']]['Title']." : " . $_REQUEST['startDate'] . " - " . $_REQUEST['endDate'] . "</h4><br />";
-        add_filter('the_title','updateReportTitle');
-        function updateReportTitle($data){
-            return $reports[$_REQUEST['rpt']]['Title']." : " . $_REQUEST['startDate'] . " - " . $_REQUEST['endDate'] ;
+    if ($_REQUEST['rpt'] == 1) {
+        $lastImport = $wpdb->get_var("SELECT MAX(sent_time) FROM pbc2.kds_detail");
+        $ret .= "<div >The last imported date is " . date("m/d/Y", strtotime($lastImport)) . ".</div>";
+    }
+    $result = $wpdb->get_results($reports[$_REQUEST['rpt']]['SQL']);
+    if ($result) {
+        $ret .= "<hr /><h4>" . $reports[$_REQUEST['rpt']]['Title'] . " : " . $_REQUEST['startDate'] . " - " . $_REQUEST['endDate'] . "</h4><br />";
+        add_filter('the_title', 'updateReportTitle');
+        function updateReportTitle($data) {
+            return $reports[$_REQUEST['rpt']]['Title'] . " : " . $_REQUEST['startDate'] . " - " . $_REQUEST['endDate'];
         }
-        $fmt = new NumberFormatter( 'en_US', NumberFormatter::CURRENCY );
-        $D['Options'][]="\"order\": [ 1, 'asc' ]";
-        $D['Options'][]="\"lengthMenu\": [ [10, 20, -1], [10, 20, \"All\"] ]";
-        $D['Headers']=$reports[$_REQUEST['rpt']]['Headers'];
+
+        $fmt = new NumberFormatter('en_US', NumberFormatter::CURRENCY);
+        $D['Options'][] = "\"order\": [ 1, 'asc' ]";
+        $D['Options'][] = "\"lengthMenu\": [ [10, 20, -1], [10, 20, \"All\"] ]";
+        $D['Headers'] = $reports[$_REQUEST['rpt']]['Headers'];
         foreach ($result as $r) {
-            if($_REQUEST['rpt']==1) {
-                $D['Results'][]=array(
+            if ($_REQUEST['rpt'] == 1) {
+                $D['Results'][] = array(
                     $r->Name,
                     $r->Weekday,
                     $r->Sales_Hour,
@@ -126,15 +132,18 @@ if(isset($_REQUEST['endDate']) && isset($_REQUEST['startDate']) && (isset($_REQU
                     $r->Customer_Count
                 );
             }
-            if($_REQUEST['rpt']==2) {
-                $D['Results'][]=array($r->Name, $r->Date_of_Business, $r->Tender, $r->Count, $r->Total);
+            if ($_REQUEST['rpt'] == 2) {
+                $D['Results'][] = array($r->Name, $r->Date_of_Business, $r->Tender, $r->Count, $r->Total);
             }
-            if($_REQUEST['rpt']==3) {
-                $D['Results'][]=array($r->restaurantName, ucwords($r->Name), $r->Date_of_Business, $r->Check, $r->Amount, $r->Ordered, $r->Arrived);
+            if ($_REQUEST['rpt'] == 3) {
+                $D['Results'][] = array($r->restaurantName, ucwords($r->Name), $r->Date_of_Business, $r->Check, $r->Amount, $r->Ordered, $r->Arrived);
+            }
+            if ($_REQUEST['rpt'] == 4) {
+                $D['Results'][] = array($r->guestName, $r->added, $r->emailAddress);
             }
         }
-        $ret.= $toast->showResultsTable($D);
-    }else{
+        $ret .= $toast->showResultsTable($D);
+    } else {
         echo '
         <div class="alert alert-warning" role="alert">
             There were no records found.
